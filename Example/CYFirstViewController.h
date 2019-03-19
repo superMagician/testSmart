951b1c35904543b386cc115ecc8d88721943691b
yy <yycodet@aliyun.com>
2019-03-19 17:49:07 +0800

    second commit

---
+++ Example/CYFirstViewController.h
@@ -0,0 +1,17 @@
+//
+//  CYFirstViewController.h
+//  testSmart_Example
+//
+//  Created by 大姚 on 2019/3/19.
+//  Copyright © 2019年 825241977@qq.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CYFirstViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Example/CYFirstViewController.m
@@ -0,0 +1,33 @@
+//
+//  CYFirstViewController.m
+//  testSmart_Example
+//
+//  Created by 大姚 on 2019/3/19.
+//  Copyright © 2019年 825241977@qq.com. All rights reserved.
+//
+
+#import "CYFirstViewController.h"
+
+@interface CYFirstViewController ()
+
+@end
+
+@implementation CYFirstViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor redColor];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
